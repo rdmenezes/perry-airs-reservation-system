6f342551cfb646e8b331e78fa6bf984aa131c0ea
xtian071989@gmail.com <xtian071989@gmail.com@04bbcbc0-d38b-76a6-a155-4374cc356f76>
2012-04-30 18:53:44 +0000

    Code Execution

---
+++ PARS/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "reservationsystem.h"
+using namespace std;
+
+int main()
+{
+    ReservationSystem chris;
+    chris.set_name();
+    chris.set_lname();
+
+    return 0;
+}
