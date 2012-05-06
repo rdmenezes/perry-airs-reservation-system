e1eac400d7567b3b3890ce84514fb18c1b1f1d56
xtian071989@gmail.com <xtian071989@gmail.com@04bbcbc0-d38b-76a6-a155-4374cc356f76>
2012-05-06 19:52:39 +0000

    airplane stuff

--- PARS/airplane.cpp
+++ PARS/airplane.cpp
@@ -2,10 +2,44 @@
 
 Airplane::Airplane()
 {
+    max_passengers = 0;
     //ctor
 }
 
 Airplane::~Airplane()
 {
     //dtor
+}
+
+// try to add passneger
+void Airplane::add_passenger()
+{
+    Passenger temp;
+    // check if max_massengers is > 30
+    if(max_passengers > 30)
+    {
+        cout << "Airplane is full";
+        exit(1);
+    }
+
+    if(max_passengers <= 30)
+    {
+        temp.set_Name();
+        temp.set_Lname();
+        temp.phone_Format();
+// push object to vector
+        passengers.push_back(temp);
+        max_passengers++;
+    }
+
+
+}
+
+//display all passenger
+void Airplane::display_list()
+{
+    for( unsigned int i = 0 ; i < passengers.size();i++ )
+    {
+        cout << passengers[i];
+    }
 }
