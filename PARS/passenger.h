0564dca48aaff7965e7af55f3388ba8bdec293ad
xtian071989@gmail.com <xtian071989@gmail.com@04bbcbc0-d38b-76a6-a155-4374cc356f76>
2012-05-06 19:49:37 +0000

    Needs a few things ... damn requirement list is huge...

--- PARS/passenger.h
+++ PARS/passenger.h
@@ -2,6 +2,7 @@
 #define PASSENGER_H
 
 #include <iostream>
+#include <cstdlib> // to clear screen
 #include <cctype>
 #include <string>
 
@@ -23,15 +24,16 @@ class Passenger
         void select_City();     //SET DESTINATION CITY.
         void select_Time();      //SELECT DEPARTURE TIME.
         void select_Date();      //SELECT DATE.
-        void confirmation_Number();      //GENERATE CONFIRMATION NUMBER
-        void phone_Format(string);    //Build (###)###-####
+        //void confirmation_Number();      //GENERATE CONFIRMATION NUMBER FROM 000000
+        void telephone_Number(string);
+        void phone_Format();    //Build (###)###-####
 
 //////////////////////////////////////////////////////////////
 ////////////////////// VALIDATIONS ///////////////////////////
 
         bool validate_names(string);      // ARE NAMES VALID?
         bool validate_telephone(string);  //IS THE PHONE VALID?
-        bool validate_credit_c(string);   //IS THE CC # VALID.
+        bool validate_credit_c(string);   //IS THE CC # VALID. IS 6 DIGITS
 
 /////////////////////////////////////////////////////////////
 ////////////////////// FETCH DATA ///////////////////////////
@@ -52,10 +54,14 @@ class Passenger
         string firstName;           //CUSTOMERS NAME.
         string lastName;            //CUSTOMERS LAST NAME.
         string phoneNumber;         //CUSTOMER PHONE #.
-        string reservation_number;  //RE #.
-        bool sel_City;
-        bool sel_Time;
-        bool sel_Date;
+        string reservation_number;  //RE #. MUST BE 6 DIGITS IN LENGTH
+        bool sel_City;              // THERE ARE 2 CITIES
+        bool sel_Time;             // FROM SFO @ 6AM AND FROM LAS @ 10PM
+        bool sel_Date;             // SELECT DATE
+        bool how_bags;             // 0 OR 1 BAG.
+        bool age;                  // age requirements
+        bool assist;               // disability assitance
+
 
 };
 
