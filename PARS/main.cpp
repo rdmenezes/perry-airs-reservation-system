8af073db660703bd1e25a88d632bbc137e98d27a
xtian071989@gmail.com <xtian071989@gmail.com@04bbcbc0-d38b-76a6-a155-4374cc356f76>
2012-05-06 19:58:19 +0000

    blachlbach

--- PARS/main.cpp
+++ PARS/main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include "passenger.h"
+#include "airplane.h"
 using namespace std;
+
 
 int main()
 {
-    /*
+/*  Testing class Passenger
     Passenger chris;
-    chris.set_name();
-    chris.set_lname();
-    cout << chris;*/
+    chris.set_Name();
+    chris.set_Lname();
+    chris.phone_Format();
+    cout << chris;
+*/
 
-
-    string phone_number;
-
-    cout << "enter phone number: ";
-    cin >> phone_number;
-
-    //if((phone_number[0] == '(') && (phone_number[3]) && (phone_number[8])
-    cout << phone_number[0] <<"," << phone_number[4] << "," << phone_number[8];
-
+// Testing class airplane
+    Airplane test;
+    test.add_passenger();
+    test.display_list();
 
 
     return 0;
